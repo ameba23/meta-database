abeb6174cc87eb31e909c677bf5abf1e50a0e577
ameba23 <ameba23@systemli.org>
2017-09-24 18:50:26 +0200

    load metadata using pyexifinfo

--- metadb.py
+++ metadb.py
@@ -1,28 +1,51 @@
 #!/usr/bin/python3
 
+# todo:
+#    sort into functions
+#    --add merge function
+#
+
 import json
 import os
 import subprocess
+import pyexifinfo as exif
 
 from pprint import pprint 
 
-#for root, dirs, files in os.walk("/home/potatoe/Musique/electro/64revolt"):
-#    for name in files:
+def importFile():
+    with open('example.json') as data_file:
+        data = json.load(data_file)
+
+def walkfiles():
+    data = []
+    for root, dirs, files in os.walk("/home/potatoe/Musique/electro/64revolt"):
+        for name in files:
+                p = os.path.join(root, name)
+                datat = exif.get_json(p)
+                print(datat[0]['File:FileSize'])
+                #datat[0]['hash'] = subprocess.Popen(["ipfs", "add", "-n", p], stdout=subprocess.PIPE)
+                #datat[0]['hash'] = subprocess.Popen(["echo", p], stdout=subprocess.PIPE)
+                datat[0]['hash'] = subprocess.check_output(['ipfs','add','-n', p])
+                print(datat[0]['hash'].split()[1])
+                data.append(datat[0])
+
 #            p = subprocess.Popen(["exiftool", "-json", os.path.join(root, name)], stdout=subprocess.PIPE, shell=True)
 #            (poutput, error) = p.communicate()
 #            print(poutput)
-
-p = subprocess.Popen(["exiftool", "-json", "/home/potatoe/Musique/electro/64revolt/Aim_for_the_flat_top/64revolt_-_Alice_Sweet_Alice.mp3"], stdout=subprocess.PIPE)
-(poutput, error) = p.communicate()
-print(poutput)
+# list
+#p = '/home/potatoe/Musique/electro/64revolt/Aim_for_the_flat_top/64revolt_-_Alice_Sweet_Alice.mp3'
+#print(datat[0]['File:FileSize'])
+#p = subprocess.Popen(["exiftool", "-json", "/home/potatoe/Musique/electro/64revolt/Aim_for_the_flat_top/64revolt_-_Alice_Sweet_Alice.mp3"], stdout=subprocess.PIPE)
+#(poutput, error) = p.communicate()
+#print(poutput)
 
 #pprint(json.load(onetime))
-with open('example.json') as data_file:
-    data = json.load(data_file)
 
-data[0]['hash'] = 404040
+
+#data[0]['hash'] = 404040
 #print(data[0]["SourceFile"]) 
-#pprint(data)
+walkfiles()
+pprint(data[5])
 #for i,v in enumerate(data[0]):
 #  print (i,v)
 
