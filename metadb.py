8b4ef0764cc871b961708193e91379daf8c93e81
ameba23 <ameba23@systemli.org>
2017-10-28 16:44:59 +0200

    added command line options and support for multiple paths

--- metadb.py
+++ metadb.py
@@ -2,36 +2,42 @@
 
 
 # todo:
-#    accept multiple arguments as file(s)
 #    allow user to define output file or stdout
-#    add command line option to omit the -n and actually add the files to IPFS 
 #    --add merge function
 #
 
 import json, os, sys
 import subprocess
+import getopt
 import pyexifinfo as exif
 
 from pprint import pprint 
 
 def usage():
-    print("Usage: ", sys.argv[0], " [path or file]")
+    print("Usage: ", sys.argv[0], "[options] [path(s) or file(s)]")
+    print(" -a, --addfiles    add files to IPFS rather than just extracting metadata")
+    print(" -h, --help        display this message")
+    print(" -v, --version     display version information and exit")
 
 def importFile():
     with open('example.json') as data_file:
         data = json.load(data_file)
     return data
 
-def exportFile():
+def exportFile(bulkfiles, outfilename):
     # write json to file.
-    with open('output.json', 'w') as outfile:  
-            stritem = json.dumps(jout, indent=4)
+    with open(outfilename, 'w') as outfile:  
+            stritem = json.dumps(bulkfiles, indent=4)
             outfile.write(stritem)
 
 def getMetadata(p, data):
     print("Extracting metadata from ", p)
     datat = exif.get_json(p)
-    datat[0]['_id'] = subprocess.check_output(['ipfs','add','-n', p]).split()[1].decode("utf-8")
+    if addfiles:
+        commandstr = ['ipfs','add', p]
+    else:
+        commandstr = ['ipfs','add','-n', p]
+    datat[0]['_id'] = subprocess.check_output(commandstr).split()[1].decode("utf-8")
     print(datat[0]['_id'])
     data.append(datat[0])
     return data
@@ -47,20 +53,54 @@ def walkfiles(argv):
     return data
 
 
-if len(sys.argv) == 2:
-    if os.path.exists(sys.argv[1]):
-        jout = walkfiles(sys.argv[1])
-        exportFile()
+try:
+    options, args = getopt.getopt(sys.argv[1:], 'avho', ['addfiles','version','help', 'output'])
+except getopt.GetoptError as err:
+    print(err)
+    usage()
+    sys.exit(2)
+
+print(options) 
+print(args)
+
+outputfile = ""
+addfiles = False
+for o, a in options:
+    if o in ("-a", "--addfiles"):
+        addfiles = True
+        print("adding files")
+    if o in ("-h", "--help"):
+        usage()
+        sys.exit()
+    if o in ("-v", "--version"):
+        print("version 0")
+        sys.exit()
+    if o in ("-o", "--output"):
+        outputfile = a
+if args == []:
+    usage()
+    sys.exit()
+
+
+jout = []
+for patharg in args:
+    if os.path.exists(patharg):
+        jout.append(walkfiles(patharg))
     else:
         usage()
         print("Path or file does not exist")
         sys.exit(2)
-else:
-    usage()
-    sys.exit(2)
 
 
-
+# if something was found
+if len(jout) > 0:
+    # wrap into a single element called docs (required by couchdb bulk docs)
+    bulkfiles = {"docs":jout}
+    if outputfile != "":
+        exportFile(bulkfiles, outputfile)
+    else:
+        #write to std out
+        pprint(bulkfiles)
 
 # broken/random code:
                 #datat[0]['hash'] = subprocess.Popen(["ipfs", "add", "-n", p], stdout=subprocess.PIPE)
